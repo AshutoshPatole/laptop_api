3514899c3824a34a830d8dfcf07969822a1c7479
AshutoshPatole <apatole479@gmail.com>
2022-11-21 09:25:16 +0530

    Add more generations and cpu

--- src/routes/admin/rankingSystem.js
+++ src/routes/admin/rankingSystem.js
@@ -2,11 +2,14 @@ import Laptop from "../../models/laptop";
 
 const rankingSystem = async (req, res) => {
   const cpu = {
-    "pentium": 2,
+    "Pentium Dual Core": 1,
+    "Pentium Silver Core": 1,
+    "Pentium Quad Core": 2,
     "Core i3": 3,
     "Core i5": 5,
     "Core i7": 7,
     "Core i9": 10,
+    "Celeron Dual Core": 2,
     "Dual Core": 2,
     "Ryzen 3 Dual Core": 3,
     "Ryzen 3 Quad Core": 4,
@@ -17,35 +20,41 @@ const rankingSystem = async (req, res) => {
   };
 
   const generation = {
-    "6": 1,
-    "7": 1,
-    "8": 2,
-    "9": 2,
-    "10": 3,
-    "11": 4,
-    "12": 5,
-    "3500": 1,
-    "4500": 2,
-    "4600": 3,
-    "5500": 4,
-    "5600": 5,
-    "3020": 1,
-    "3250": 1,
-    "4200": 3,
-    "5200": 4,
-    "5425": 5,
-    "3700": 1,
-    "4700": 2,
-    "4800": 3,
-    "5700": 4,
-    "5800": 5,
+    6: 1,
+    "N4500": 1,
+    "N5030": 1,
+    "N4020": 1,
+    7: 2,
+    8: 4,
+    9: 4,
+    10: 6,
+    11: 8,
+    12: 10,
+    3500: 1,
+    3450: 1,
+    4500: 2,
+    4600: 3,
+    5500: 5,
+    5600: 6,
+    5625: 5,
+    3020: 2,
+    3250: 3,
+    4200: 4,
+    5200: 4,
+    5425: 5,
+    3700: 4,
+    4700: 4,
+    4800: 5,
+    5700: 7,
+    5800: 8,
+    5900: 9,
+    6600: 10,
+    6800: 11,
+    6900: 12
   };
-//   console.log(generation["12"])
-
   var laptops = await Laptop.find({});
-  var invalid = []
+  var invalid = [];
   for (var i = 0; i < laptops.length; i++) {
-
     var cpu_name = laptops[i]["processor_name"];
     var cpu_brand = laptops[i]["processor_brand"];
     var cpu_generation = laptops[i]["processor_variant"];
@@ -54,21 +63,25 @@ const rankingSystem = async (req, res) => {
 
     if (cpu_generation !== undefined) {
       if (cpu_brand == "Intel") {
-        gen = cpu_generation.substring(0, 2);
-        total_score = cpu[cpu_name] + generation[gen];
+        gen = cpu_generation.substring(0, 1);
+        if (gen <= 9 && gen >= 6) {
+          total_score = cpu[cpu_name] + generation[gen];
+        } else {
+          var new_gen = cpu_generation.substring(0, 2);
+          total_score = cpu[cpu_name] + generation[new_gen];
+        }
       } else {
         gen = cpu_generation.substring(0, 4);
         total_score = cpu[cpu_name] + generation[gen];
       }
     }
 
-    if(Number.isNaN(total_score) || total_score == undefined){
-        invalid.push("invalid")
-        console.log(`score NAN: ${laptops[i]['_id']}`);
-        total_score = 0;
+    if (Number.isNaN(total_score) || total_score == undefined) {
+      invalid.push("invalid");
+      console.log(`score NAN: ${laptops[i]["_id"]}`);
+      total_score = 0;
     }
 
-    // console.log(total_score);
     try {
       await Laptop.findByIdAndUpdate(
         laptops[i]["id"],
@@ -81,8 +94,8 @@ const rankingSystem = async (req, res) => {
       throw e;
     }
   }
-
-  res.send(invalid.length);
+  // TODO: Fix intel cpu generation score count logic: total 168 laptops affected
+  res.send(`Invalid Count ${invalid.length}`);
 };
 
 export default rankingSystem;
