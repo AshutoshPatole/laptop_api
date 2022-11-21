006bbd698b07afd0b3c8f9a9c261ea85ef17f50d
AshutoshPatole <apatole479@gmail.com>
2022-11-21 09:47:17 +0530

    refactor ranking system

--- src/routes/admin/rankingSystem.js
+++ src/routes/admin/rankingSystem.js
@@ -1,63 +1,7 @@
 import Laptop from "../../models/laptop";
+import { cpu, generation } from "../../constants/scores";
 
 const rankingSystem = async (req, res) => {
-  const cpu = {
-    "Pentium Dual Core": 1,
-    "Pentium Silver": 1,
-    "Pentium Quad Core": 2,
-    "Core i3": 4,
-    "Core i5": 6,
-    "Core i7": 8,
-    "Core i9": 12,
-    "Celeron Dual Core": 2,
-    "Dual Core": 2,
-    "Ryzen 3 Dual Core": 3,
-    "Ryzen 3 Quad Core": 4,
-    "Ryzen 5 Dual Core": 3,
-    "Ryzen 5 Quad Core": 5,
-    "Ryzen 5 Hexa Core": 6,
-    "Ryzen 7 Dual Core": 4,
-    "Ryzen 7 Quad Core": 6,
-    "Ryzen 7 Hexa Core": 8,
-    "Ryzen 7 Octa Core": 10,
-    "Ryzen 9 Octa Core": 12,
-  };
-
-  const generation = {
-    6: 1,
-    "N4500": 1,
-    "N5030": 1,
-    "N4020": 1,
-    7: 2,
-    8: 4,
-    9: 4,
-    10: 6,
-    11: 8,
-    12: 10,
-    3020: 1,
-    3250: 2,
-    3450: 2,
-    3500: 3,
-    3700: 4,
-    4200: 4,
-    4500: 4,
-    4600: 5,
-    4700: 5,
-    4800: 6,
-    5200: 3,
-    5300: 4,
-    5425: 4,
-    5500: 5,
-    5600: 6,
-    5625: 6,
-    5700: 7,
-    5800: 8,
-    5825: 8,
-    5900: 9,
-    6600: 10,
-    6800: 11,
-    6900: 12
-  };
   var laptops = await Laptop.find({});
   var invalid = [];
   for (var i = 0; i < laptops.length; i++) {
