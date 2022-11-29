d6f15affb98e4d6645d6182305f5406d37d677df
AshutoshPatole <apatole479@gmail.com>
2022-11-29 10:22:25 +0530

    Remove unused files

--- src/middleware/isAdmin.js
+++ src/middleware/isAdmin.js
@@ -1,27 +1,29 @@
-import STATUS_CODES from "../constants/statusCode";
-import Users from "../models/user";
+import STATUS_CODES from '../constants/statusCode'
+import Users from '../models/user'
 
 // !TODO: Fix broken isAdmin logic. How to get uid of user without passing it along the request.
 const isAdmin = async (req, res, next) => {
     try {
+        console.log(req.user)
         const role = await Users.findById(req.user.id)
-    
+
         if (role['isAdmin']) {
-          next();
+            next()
         } else {
-          return res.status(STATUS_CODES.FORBIDDEN).json({
-            error: {
-              message: 'You are not permitted to access this resource'
-            }
-          });
+            return res.status(STATUS_CODES.FORBIDDEN).json({
+                error: {
+                    message: 'You are not permitted to access this resource',
+                },
+            })
         }
-      } catch(error) {
+    } catch (error) {
         return res.status(STATUS_CODES.INTERNAL_SERVER_ERROR).json({
-          error: {
-            message: 'An error occurred while getting user access. Please try again'
-          }
-        });
-      }
+            error: {
+                message:
+                    'An error occurred while getting user access. Please try again',
+            },
+        })
+    }
 }
 
 export default isAdmin
