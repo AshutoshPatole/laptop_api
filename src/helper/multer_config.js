1dfe89d4eff821d1b1e9deaf2c8df47f652a36a3
AshutoshPatole <apatole479@gmail.com>
2023-02-14 13:42:56 +0530

    Add comments for multer config

--- src/helper/multer_config.js
+++ src/helper/multer_config.js
@@ -1,16 +1,23 @@
+/* 
+This file contains configuration for storing files uploaded from '/admin/upload' endpoint
+*/
+
 import multer from 'multer'
 import path from 'path'
 
+// Using the diskStorage for storing files in uploads directory
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, path.join(__dirname, '../uploads'))
     },
+    // the filename will be <date>.json format. This format is used by '/admin/update-pricing' endpoint
     filename: function (req, file, cb) {
         const currentDate = new Date().toISOString().slice(0, 10)
         cb(null, currentDate + '.json')
     },
 })
 
+// only json files are allowed
 const fileFilter = function (req, file, cb) {
     const extname = path.extname(file.originalname)
     if (extname !== '.json') {
