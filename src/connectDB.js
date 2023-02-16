887bcaa8e2eeb344c08c38bf89c40b6f867a3d3f
AshutoshPatole <apatole479@gmail.com>
2023-02-16 17:41:18 +0530

    Remove road blockers for development

--- src/connectDB.js
+++ src/connectDB.js
@@ -2,7 +2,6 @@ import mongoose from 'mongoose'
 
 const connectDatabase = () => {
     try {
-        mongoose.set('strictQuery', true)
         mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
--- src/routes/admin/index.js
+++ src/routes/admin/index.js
@@ -4,10 +4,8 @@ import analytics from './analytics'
 import bulkInsert from './bulk-upload'
 import rankingSystem from './rankingSystem'
 import updatePricing from './updatePricing'
-import verifyToken from '../../middleware/verifyToken'
 import createAdminUser from './createAdmin'
 import getDuplicates from './fetchDuplicates'
-import isAdmin from '../../middleware/isAdmin'
 import multer from 'multer'
 import {
     pricingStore,
@@ -18,17 +16,15 @@ import uploadFile from './fileUpload'
 
 const adminRouter = Router()
 
-// two multer config for saving new laptop file and updated pricing file
 const newLaptopFile = multer({
     storage: newLaptopsStore,
     fileFilter: fileFilter,
 })
 
-// ! TODO: Fix file not uploading using this configuration. It works on newLaptop file
 const pricingFile = multer({ storage: pricingStore, fileFilter: fileFilter })
 
-adminRouter.get('/ranking', verifyToken, rankingSystem)
-adminRouter.get('/analytics', verifyToken, cache.route(), analytics)
+adminRouter.get('/ranking', rankingSystem)
+adminRouter.get('/analytics', cache.route(), analytics)
 adminRouter.get('/update-pricing', updatePricing)
 adminRouter.get('/bulk-insert', bulkInsert)
 adminRouter.post(
@@ -37,7 +33,7 @@ adminRouter.post(
     uploadFile
 )
 adminRouter.get('/update', updatePricing)
-adminRouter.get('/duplicate', /* verifyToken, isAdmin, */ getDuplicates)
+adminRouter.get('/duplicate', getDuplicates)
 adminRouter.post('/create-user', createAdminUser)
 adminRouter.post('/upload', pricingFile.single('file'), uploadFile)
 export default adminRouter
