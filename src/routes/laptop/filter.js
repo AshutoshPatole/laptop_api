ea3b5928ed5072870c26ca79cbbd24357cd28802
AshutoshPatole <apatole479@gmail.com>
2023-02-18 10:43:15 +0530

    Add doc for filter endpoint

--- src/routes/laptop/filter.js
+++ src/routes/laptop/filter.js
@@ -2,8 +2,13 @@
 
 This endpoint will filter laptops based on price, cpuScore, memoryScore, multimediaScore, connectivityScore and so on.
 
+http://localhost:8000/laptop/filter?maxPrice=80000&cpuScore=1
+
+All fields are optional: send 1 for low end and -1 for high end laptops. Example cpuScore=-1
 
 */
+
+// !  TODO: Implement other fields like graphics and portability
 
 import Laptop from '../../models/laptop'
 
